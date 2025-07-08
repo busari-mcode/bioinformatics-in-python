e1f687db47efb08c55005c10822111c6ea8e73ca
Busari Moruf <busarimoruf20@gmail.com>
2025-07-08 07:19:22 -0400

    new changes

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -8,4 +8,27 @@ def validateSeq(dna_seq):
     for nuc in tmpseq:
         if nuc not in Nucleotides:
             return False
-        return tmpseq
+    return tmpseq
+
+
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+
+
+# ************************ LEARNING PHASE 1 ******************************
+# ****************Validate DNA Sequence*************
+
+# # DNA Toolkit file
+
+# Nucleotides = ["A", "C", "G", "T"]
+
+# # Check the sequence to make sure it is a DNA String
+# def validateSeq(dna_seq):
+#     tmpseq = dna_seq.upper()
+#     for nuc in tmpseq:
+#         if nuc not in Nucleotides:
+#             return False
+#     return tmpseq
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 # DNA Toolset/Code testing file
 from DNAToolkit import *
+import random
 
-rndDNAStr = "ATTTCGT"
+# Creating a random DNA Sequence for testing:
+randDNAStr = ''.join([random.choice(Nucleotides)
+                      for nuc in range(50)])
 
-print(validateSeq(rndDNAStr))
+print(validateSeq(randDNAStr))
+print(countNucFrequency(randDNAStr))
+
+
+# ************************ LEARNING PHASE 1 ******************************
+# ****************Validate DNA Sequence*************
+
+# # DNA Toolset/Code testing file
+# from DNAToolkit import *
+
+# # random DNA string
+# rndDNAStr = "ATTTCGT"
+# rndDNAStr2 = "ATTTCGTX"
+# rndDNAStr3 = "AtCCgGGtGGt"
+
+# print(validateSeq(rndDNAStr))
+# print(validateSeq(rndDNAStr2))
+# print(validateSeq(rndDNAStr3))
