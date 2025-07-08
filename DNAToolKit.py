db9340d7b2e697cd1e1fd67979921cf872df239b
Busari Moruf <busarimoruf20@gmail.com>
2025-07-07 23:38:38 -0400

    new changes

---
+++ DNAToolKit.py
@@ -0,0 +1,11 @@
+# DNA Toolkit file
+
+Nucleotides = ["A", "C", "G", "T"]
+
+# Check the sequence to make sure it is a DNA String
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+        return tmpseq
