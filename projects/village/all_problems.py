e14dc2918f7491e5c39ef3a2ec67c23ddcf1a59a
Busari Moruf <busarimoruf20@gmail.com>
2025-07-11 19:24:37 -0400

    new changes

--- projects/village/all_problems.py
+++ projects/village/all_problems.py
@@ -7,7 +7,7 @@ wordTwoEndPos = 102
 
 txtStr = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain"
 
-# Note: end position is not inclusive, so we add 1 to capture it
+        # Note: end position is not inclusive, so we add 1 to capture it
 print(
     f'{txtStr[wordOneStartPos:wordOneEndPos + 1]} {txtStr[wordTwoStartPos:wordTwoEndPos + 1]}'
 )
