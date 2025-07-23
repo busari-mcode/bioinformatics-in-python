d132b22604ca6f0bf522e9f78ca74c5730a206de
Busari Moruf <busarimoruf20@gmail.com>
2025-07-22 22:20:00 -0700

    new changes

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -1,11 +1,3 @@
-# *************************************** LEARNING PHASE 3 *******************************************
-# ****************Rosalind problems: Counting DNA Nucleotides*************
-
-
-
-# *************************************** LEARNING PHASE 2 *******************************************
-# ****************Counting Nucleotides in the Validated random DNA String generated*************
-
 # DNA Toolkit file
 import collections
 
@@ -29,6 +21,45 @@ def countNucFrequency(seq):
     # the four lines of codes above can be replaced by the line below here 
     # using the collection module
     # return dict(collections.Counter(seq))
+
+def transcription(seq):
+    # DNA -> RNA Transcription
+    return seq.replace("T", "U")
+
+
+
+
+
+
+
+
+
+# *************************************** LEARNING PHASE 2 *******************************************
+# ****************Counting Nucleotides in the Validated random DNA String generated*************
+
+# # DNA Toolkit file
+# import collections
+
+# Nucleotides = ["A", "C", "G", "T"]
+
+# # Check the sequence to make sure it is a DNA String
+# def validateSeq(dna_seq):
+#     tmpseq = dna_seq.upper()
+#     for nuc in tmpseq:
+#         if nuc not in Nucleotides:
+#             return False
+#     return tmpseq
+
+
+# def countNucFrequency(seq):
+#     tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+#     for nuc in seq:
+#         tmpFreqDict[nuc] += 1
+#     return tmpFreqDict
+
+#     # the four lines of codes above can be replaced by the line below here 
+#     # using the collection module
+#     # return dict(collections.Counter(seq))
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# *************************************** LEARNING PHASE 3 *******************************************
-# ****************Rosalind problems: Counting DNA Nucleotides*************
-
-
-
-# *************************************** LEARNING PHASE 2 *******************************************
-# ****************Counting Nucleotides in the Validated random DNA String generated*************
-
 # DNA Toolset/Code testing file
 from DNAToolkit import *
 import random
@@ -15,7 +7,43 @@ randDNAStr = ''.join([random.choice(Nucleotides)
                       for nuc in range(50)])
 
 DNAStr = validateSeq(randDNAStr)
-print(countNucFrequency(DNAStr))
+
+print(f'\nSequence: {DNAStr}\n')
+print(f'[1] + Sequence Length: {len(DNAStr)}\n')
+print(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
+print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+
+
+
+
+# *************************************** LEARNING PHASE 3 *******************************************
+# # DNA Toolset/Code testing file
+# from DNAToolkit import *
+# import random
+
+# # Creating a random DNA Sequence for testing:
+# randDNAStr = ''.join([random.choice(Nucleotides)
+#                       for nuc in range(50)])
+
+# DNAStr = validateSeq(randDNAStr)
+# print(countNucFrequency(DNAStr))
+# print(transcription(DNAStr))
+
+
+
+# *************************************** LEARNING PHASE 2 *******************************************
+# ****************Counting Nucleotides in the Validated random DNA String generated*************
+
+# # DNA Toolset/Code testing file
+# from DNAToolkit import *
+# import random
+
+# # Creating a random DNA Sequence for testing:
+# randDNAStr = ''.join([random.choice(Nucleotides)
+#                       for nuc in range(50)])
+
+# DNAStr = validateSeq(randDNAStr)
+# print(countNucFrequency(DNAStr))
 
 
 # ************************ LEARNING PHASE 1 ******************************
