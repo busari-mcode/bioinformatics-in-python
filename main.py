dca28fc802a94cafbf6359e17947c55762f9e20b
Busari Moruf <busarimoruf20@gmail.com>
2025-07-28 00:18:17 -0700

    new changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # DNA Toolset/Code testing file
 from DNAToolkit import *
+from utilities import colored
 import random
 
 # Creating a random DNA Sequence for testing:
@@ -8,14 +9,14 @@ randDNAStr = ''.join([random.choice(Nucleotides)
 
 DNAStr = validateSeq(randDNAStr)
 
-print(f'\nSequence: {DNAStr}\n')
+print(f'\nSequence: {colored(DNAStr)}\n')
 print(f'[1] + Sequence Length: {len(DNAStr)}\n')
-print(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
-print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
+print(colored(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n'))
+print(f'[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}\n')
 
-print(f"[4] + DNA String + Reverse Complement:\n5' {DNAStr} 3'")
+print(f"[4] + DNA String + Reverse Complement:\n5' {colored(DNAStr)} 3'")
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
-print(f"3' {reverse_complement(DNAStr)} 5'\n")
+print(f"3' {colored(reverse_complement(DNAStr))} 5'\n")
 
 # *************************************** LEARNING PHASE 3 *******************************************
 # # DNA Toolset/Code testing file
